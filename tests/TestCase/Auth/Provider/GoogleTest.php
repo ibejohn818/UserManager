a1ddbf574a27ab457b7d3a931ba17d44f71b3a66
John Hardy <john.hardy@me.com>
2017-11-19 15:16:20 -0800

    add files

---
+++ tests/TestCase/Auth/Provider/GoogleTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace UserManager\Test\TestCase\Auth;
+
+use UserManager\Auth\Provider\Google;
+use Cake\TestSuite\TestCase;
+use Cake\Core\Configure;
+
+class GoogleProviderTest extends TestCase
+{
+
+
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        //$config = TableRegistry::exists('UserAccounts') ? [] : ['className' => UserAccountsTable::class];
+        //$this->UserAccountsTable = TableRegistry::get('UserAccounts', $config);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->UserAccountsTable);
+
+        parent::tearDown();
+    }
+
+
+    public function testSetClient()
+    {
+
+        $mock = $this->getMockBuilder("\Google_Client")
+                        ->getMock();
+
+        $g = new Google();
+
+        $g->setClient($mock);
+
+        $c = $g->client();
+
+        $this->assertTrue(($c instanceof \Google_Client));
+
+    }
+
+    public function testClient()
+    {
+
+        $_SERVER['HTTP_HOST'] = 'test.com';
+        $_SERVER['HTTPS'] = 'on';
+
+        $g = new Google();
+
+        $c = $g->client();
+
+        $this->assertEquals($c->getRedirectUri(), "https://test.com");
+
+    }
+
+
+    public function testHandleLoginRedirect()
+    {
+
+        $this->markTestIncomplete('Not implemented yet.');
+        $mock = $this->getMockBuilder("\Google_Client")
+                        ->getMock();
+
+        $g = new Google();
+
+        $g->setClient($mock);
+
+        $c = $g->client();
+
+
+    }
+}
