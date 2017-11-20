9e0cd91c42ea52a956e18e389fc4356e3983b553
John Hardy <john.hardy@me.com>
2017-11-19 19:36:11 -0800

    Update Auth Tests

--- tests/TestCase/Auth/Provider/GoogleTest.php
+++ tests/TestCase/Auth/Provider/GoogleTest.php
@@ -10,6 +10,17 @@ class GoogleProviderTest extends TestCase
 {
 
 
+    public $fixtures = [
+        'plugin.user_manager.user_accounts',
+        'plugin.user_manager.user_account_custom_field_values',
+        'plugin.user_manager.user_account_custom_fields',
+        'plugin.user_manager.user_account_login_provider_data',
+        'plugin.user_manager.user_account_groups',
+        'plugin.user_manager.user_account_group_assignments',
+        'plugin.user_manager.user_account_permissions',
+        'plugin.user_manager.user_account_passwds',
+        'plugin.user_manager.user_account_profile_images',
+    ];
 
     /**
      * setUp method
@@ -89,6 +100,20 @@ class GoogleProviderTest extends TestCase
 
     }
 
+    public function testOauthService()
+    {
+
+        $_SERVER['HTTP_HOST'] = 'test.com';
+        $_SERVER['HTTPS'] = 'on';
+
+        $g = new Google();
+
+        $res = $g->oauthService();
+
+        $this->assertTrue(($res instanceof \Google_Service_Oauth2));
+
+    }
+
     public function testHandleLoginRedirect()
     {
 
@@ -116,7 +141,47 @@ class GoogleProviderTest extends TestCase
         $this->assertEquals($res['token'], 'token');
 
     }
+
+    public function testAuthenticate()
+    {
+
+
+        $mockClient = $this->getMockBuilder("\Google_Client")
+                            ->getMock();
+
+        $mockClient->expects($this->once())
+                    ->method("getAccessToken")
+                    ->will($this->returnValue("token"));
+
+        $mockService = $this->getMockBuilder("\Google_Service_Oauth2")
+                            ->setConstructorArgs([$mockClient])
+                            ->getMock();
+
+        $mockService->userinfo = new userinfo_obj();
+
+        $mockReq = $this->getMockBuilder("\Cake\Http\ServerRequest")
+                        ->getMock();
+
+        $mockReq->query = ['code'=>'token'];
+
+        $mockRes = $this->getMockBuilder("\Cake\Http\Response")
+                        ->getMock();
+
+        $g = new Google();
+
+        $g->setClient($mockClient);
+        $g->setOauthService($mockService);
+
+        $res = $g->authenticate($mockReq, $mockRes);
+
+        $this->assertEquals($res['id'], 1);
+        $this->assertEquals($res['profile_uri'] , "john.html");
+
+    }
+
 }
+
+
 class userinfo
 {
     public function get()
@@ -125,3 +190,18 @@ class userinfo
     }
 }
 
+class userinfo_obj
+{
+    public function get()
+    {
+        $cls = new \stdclass();
+
+        $cls->id = "id";
+        $cls->picture = "picture";
+        $cls->email = "jhardy@test.com";
+        $cls->givenName = "givenName";
+        $cls->familyName = "familyName";
+
+        return $cls;
+    }
+}
