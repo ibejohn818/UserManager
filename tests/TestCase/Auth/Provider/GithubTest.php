4ec12d6a59103d5574c69c2cc0d2a25e44c8db6f
John <john.hardy@me.com>
2017-11-26 20:14:31 -0800

    Update github provider tests

--- tests/TestCase/Auth/Provider/GithubTest.php
+++ tests/TestCase/Auth/Provider/GithubTest.php
@@ -9,6 +9,17 @@ use Cake\Core\Configure;
 class GithubTest extends TestCase
 {
 
+    public $fixtures = [
+        'plugin.user_manager.user_account_permissions',
+        'plugin.user_manager.user_account_groups',
+        'plugin.user_manager.user_account_group_assignments',
+        'plugin.user_manager.user_accounts',
+        'plugin.user_manager.user_account_custom_field_values',
+        'plugin.user_manager.user_account_custom_fields',
+        'plugin.user_manager.user_account_login_provider_data',
+        'plugin.user_manager.user_account_passwds',
+        'plugin.user_manager.user_account_profile_images',
+    ];
 
     public function setUp()
     {
@@ -120,10 +131,7 @@ class GithubTest extends TestCase
     {
 
 
-        $req = new \Cake\Http\ServerRequest();
-
-        $req->query['code'] = "test";
-
+        //test result not cache hit, write to cache
         $conf = ["cacheAdapter"=> __NAMESPACE__."\\getTestCacheMock"];
 
         $gh = new Github($conf);
@@ -144,10 +152,220 @@ class GithubTest extends TestCase
         $this->assertEquals(getTestCacheMock::$write['etag'], "Test-Tag");
         $this->assertEquals(getTestCacheMock::$write['modified'], "Test-Modified");
         $this->assertEquals(getTestCacheMock::$write['result']['content']['test'], "test");
+
+        //test result cached
+        $conf = ["cacheAdapter"=> __NAMESPACE__."\\getTestCacheMockCached"];
+
+        $gh = new Github($conf);
+
+        $mockClient = $this->getMockBuilder('\Cake\Network\Http\Client')
+						->disableOriginalConstructor()
+						->setMethods(['get'])
+                        ->getMock();
+
+        $mockClient->method('get')->willReturn(new getTestGetBodyCached());
+
+        $gh->httpClient($mockClient);
+        $gh->accessToken("test-token");
+
+        $res = $gh->get("/cached", ['cached'=>'cached'], [], true);
+
+        $this->assertEquals($res['content']['test-cached'], 'cached-result');
+
+
+        $gh = Github::personalToken("test-token");
+
+        $mockClient = $this->getMockBuilder('\Cake\Network\Http\Client')
+						->disableOriginalConstructor()
+						->setMethods(['get'])
+                        ->getMock();
+
+        $mockClient->method('get')->willReturn(new getTestGetBodyThird());
+
+        $gh->httpClient($mockClient);
+
+        $res = $gh->get("/test-result", [], [], false);
+
+        $this->assertEquals($res['content']['test'], 'test-result');
+        $this->assertEquals($res['pagination']['total_pages'], 34);
+        $this->assertTrue(isset($res['pagination']['next']));
+        $this->assertTrue(isset($res['pagination']['last']));
+
+        //test first prev pagination links
+        $gh = Github::personalToken("test-token");
+
+        $mockClient = $this->getMockBuilder('\Cake\Network\Http\Client')
+						->disableOriginalConstructor()
+						->setMethods(['get'])
+                        ->getMock();
+
+        $mockClient->method('get')->willReturn(new getTestGetBodyForth());
+
+
+        $gh->httpClient($mockClient);
+
+        $res = $gh->get("/test-result", [], [], false);
+
+        $this->assertEquals($res['content']['test'], 'test-forth');
+        $this->assertEquals($res['pagination']['total_pages'], 21);
+        $this->assertTrue(isset($res['pagination']['first']));
+        $this->assertTrue(isset($res['pagination']['prev']));
+
+    }
+
+    public function testAuthenticate()
+    {
+
+        \Cake\Cache\Cache::disable();
+
+        $req = new \Cake\Http\ServerRequest();
+        $res = new \Cake\Http\Response();
+
+        $req->query['code'] = 'test-token';
+
+        $mockClient = $this->getMockBuilder('\Cake\Network\Http\Client')
+						->disableOriginalConstructor()
+						->setMethods(['get', 'post'])
+                        ->getMock();
+
+        $mockClient->method('get')->willReturn(new getTestAuthenticateBody());
+        $mockClient->method('post')->willReturn(new postTestAuthenticateBody());
+
+        $gh = new Github();
+
+        $gh->httpClient($mockClient);
+
+        $creds = $gh->authenticate($req, $res);
+
+        $this->assertEquals($creds['id'], 1);
+        $this->assertEquals($creds['email'], 'jhardy@test.com');
     }
 
 }
 
+class getTestAuthenticateBodyTwo
+{
+
+    public $code = 200;
+    public $headers = [
+        'Etag'=>'etag'
+    ];
+
+    public function body()
+    {
+        return json_encode([
+            'email'=>'jtest@test.com',
+            'id'=>'6969',
+            'name'=>'John',
+            'login'=>'ibejohn818'
+        ]);
+    }
+
+}
+class getTestAuthenticateBody
+{
+
+    public $code = 200;
+    public $headers = [
+        'Etag'=>'etag'
+    ];
+
+    public function body()
+    {
+        return json_encode([
+            'email'=>'jhardy@test.com',
+            'id'=>'6969',
+            'name'=>'John Test',
+            'login'=>'ibejohn818'
+        ]);
+    }
+
+}
+
+class postTestAuthenticateBody
+{
+
+    public $code= 200;
+
+    public function body()
+    {
+        return 'access_token=test-token&scope=test-scope&token_type=test-type';
+    }
+
+}
+
+class getTestGetBodyForth
+{
+    public $code = 200;
+    public $headers = [
+        'Etag'=>'test-etag',
+        'Last-Modified'=>'test-modified',
+        'Link'=>'<https://api.github.com/search/code?q=addClass+user%3Amozilla&page=1>; rel="first",
+                <https://api.github.com/search/code?q=addClass+user%3Amozilla&page=20>; rel="prev"'
+    ];
+    public function body()
+    {
+        return json_encode([
+            'test'=>'test-forth'
+        ]);
+    }
+
+}
+
+class getTestGetBodyThird
+{
+    public $code = 200;
+    public $headers = [
+        'Etag'=>'test-etag',
+        'Last-Modified'=>'test-modified',
+        'Link'=>'<https://api.github.com/search/code?q=addClass+user%3Amozilla&page=2>; rel="next",
+                <https://api.github.com/search/code?q=addClass+user%3Amozilla&page=34>; rel="last"'
+    ];
+    public function body()
+    {
+        return json_encode([
+            'test'=>'test-result'
+        ]);
+    }
+
+}
+
+class getTestGetBodyCached
+{
+    public $code = 304;
+    public $headers = [
+            'Etag'=>'Cached-Tag',
+            'Last-Modified'=>'Cached-Modified',
+            'Link'=>[
+            ]
+    ];
+
+}
+
+//testGet set 2
+class getTestCacheMockCached
+{
+    public static $write;
+    public static $result = [
+        'result'=>[
+            'content'=>[
+                'test-cached'=>'cached-result'
+            ]
+        ],
+        'headers'=>[
+        ],
+        'etag'=>'cached-etag',
+        'modified'=>'cached-modified'
+    ];
+
+    public static function read()
+    {
+        return static::$result;
+    }
+}
+
+
+//testGet set1
 class getTestCacheMock
 {
     public static $write = null;
@@ -161,6 +379,7 @@ class getTestCacheMock
     }
 }
 
+//testGet set1
 class getTestGetBody
 {
     public $code = '200';
