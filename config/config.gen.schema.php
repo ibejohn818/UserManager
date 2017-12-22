4df637adbf6d95ea7f2208b93978733bf65b007f
John <john.hardy@me.com>
2017-12-21 17:47:12 -0800

    update locate account test

--- config/config.gen.schema.php
+++ config/config.gen.schema.php
@@ -2,12 +2,22 @@
 
 $gen = [
     'User-Registration'=>[
-        'help'=>'',
+        'help'=>'User registration options',
         'settings'=>[
             [
                 'help'=>'Require user registration to verify email address',
                 'key'=>'emailRegistration',
                 'default'=>0
+            ]
+        ]
+    ],
+    'User-Password'=>[
+        'help'=>'User password options',
+        'settings'=>[
+            [
+                'help'=>'Number of passwords to maintain in history. 0=Infinite',
+                'key'=>'userPasswdHistoryCount',
+                'default'=>5
             ]
         ]
     ]
